e15240762e415f346f593f2b71b5463253839fb8
haruna666 <sumiti6hal@gmail.com>
2020-11-24 21:29:30 +0900

    users コントローラーの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
 	before_action :basic_auth
+	before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-	private
+  private
 
 	def basic_auth
 		authenticate_or_request_with_http_basic do |username, password|
 			username == ENV["BASIC_AUTH_NAME"] && password == ENV["BASIC_AUTH_PASS"]
 		end
 	end
+
+	def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :mail, :password, :password_confirmation, :last_name, :first_name, :furigana_last, :furigana_first, :date_of_birth])
+  end
 end
