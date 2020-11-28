46c543fa7331623d98df0ea57fd6dd11c6fcabdc
haruna666 <sumiti6hal@gmail.com>
2020-11-28 19:37:48 +0900

    item単体テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name {"帽子"}
+    price {"500"}
+    detail {"可愛いです"}
+    category_id {"レディース"}
+    quality_id {"新品・未使用"}
+    prefecture_id {"北海道"}
+    shipped_date_id {"1~2日で発送"}
+    shipment_burden_id {"着払い（購入者負担）"}
+    user_id {"2"}
   end
 end
