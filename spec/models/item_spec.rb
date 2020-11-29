7c0f88d4badf704bcab13c6a32aee03b66db3b6a
haruna666 <sumiti6hal@gmail.com>
2020-11-29 12:04:39 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -21,35 +21,50 @@ describe Item do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price cant't be blank")
       end
-      it 'category_idが空だとうまくいかない' do
+      it 'priceが299円以下だと登録できない' do
+        @item.price = <299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be under299yen")
+      end
+      it 'priceが10000000円以上だと登録できない' do
+        @item.price = >10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price cant't be over10000000yen")
+      end
+      it 'category_idが１以外でないと登録できない' do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category_id cant't be blank")
       end
-      it 'quality_idが空だとうまくいかない' do
+      it 'quality_idが１以外でないと登録できない' do
         @item.quality_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Quality_id cant't be blank")
       end
-      it 'prefecture_idが空だとうまくいかない' do
+      it 'prefecture_idが１以外でないと登録できない' do
         @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture_id cant't be blank")
       end
-      it 'shipped_date_idが空だとうまくいかない' do
+      it 'shipped_date_idが１以外でないと登録できない' do
         @item.shipped_date_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipped_date_id cant't be blank")
       end
-      it 'shipment_burden_idが空だとうまくいかない' do
+      it 'shipment_burden_idが１以外でないと登録できない' do
         @item.shipment_burden_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipment_burden_id cant't be blank")
       end
-      it 'user_idが空だとうまくいかない' do
+      it 'user_idが１以外でないと登録できない' do
         @item.user_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("User_id cant't be blank")
+      end
+      it 'category_idは、１以外でないと登録できない' do
+        @item.shipped_date_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipped_date_id cant't be blank")
       end
     end
   end
