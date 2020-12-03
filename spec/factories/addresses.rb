9fa3e5db2630e885f06aec8cc0737f36243baa1f
haruna666 <sumiti6hal@gmail.com>
2020-12-03 20:35:20 +0900

    テストコードの準備

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :address do
+    postal_coad { 123-4567 }
+    prefecture { 3 }
+    city { "あああああ" }
+    address { 123 }
+    phone_number { 09012345678 }
+  end
+end
---
+++ spec/factories/orders.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :order do
+    name {"ああああ"}
+    price { 3000 }
+    shipment_burden_id { 4 }
+  end
+end
