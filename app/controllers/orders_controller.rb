1565129cdad80c81241fe07e68b573707a3317a2
haruna666 <sumiti6hal@gmail.com>
2020-12-06 12:01:09 +0900

    orderコントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,25 +8,26 @@ class OrdersController < ApplicationController
 	end
 
 	def create
+		@item = Item.find(params[:item_id])
 		@userorder = UserOrder.new(order_params)
 		if @userorder.valid?
-			Payjp.api_key = "sk_test_f7e22c0b893dd0a05fc2626f"
-			Payjp::Charge.create(
-				amount: order_params[:price],
-				card: order_params[:token],
-				currency: 'jpy'
-			)
+			# Payjp.api_key = "sk_test_f7e22c0b893dd0a05fc2626f"
+			# Payjp::Charge.create(
+			# 	amount: order_params[:price],
+			# 	card: order_params[:token],
+			# 	currency: 'jpy'
+			# )
 			@userorder.save
 			return redirect_to root_path
 		else
-			redirect_to root_path
+			render :index
 		end
 	end
 
 	private
 
 	def order_params
-		params[:userorder].permit(:item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(token: params[:token], user_id: current_user.id)
+		params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(token: params[:token], user_id: current_user.id)
 	end
 
 	def move_to_index
