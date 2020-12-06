65140067066432dc23d237ea4eebdba5e8c9fd32
haruna666 <sumiti6hal@gmail.com>
2020-12-06 21:47:04 +0900

    soldoutの条件文（コントローラー、ビュー）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  has_one :order
   has_one_attached :image
   belongs_to :category
   belongs_to :quality
   belongs_to :prefecture
   belongs_to :shipped_date
   belongs_to :shipment_burden
+
 
   with_options presence: true do
     validates :name
