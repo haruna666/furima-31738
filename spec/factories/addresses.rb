9fdebe8c32f78892eb9c7436e380e6d4888728fb
haruna666 <sumiti6hal@gmail.com>
2020-12-07 12:26:35 +0900

    テストコード編集

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
   factory :address do
-    postal_code { "123-4567" }
-    prefecture_id { 3 }
-    city { "あああああ" }
-    address { "あああ１ー２" }
-    phone_number {"09012345678" }
+    
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,57 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  before do
-    @address = FactoryBot.build(:address)
-  end
-
-  describe '商品購入' do
-    context '商品購入がうまくいくとき' do
-      it 'postal_code,prefecture_id,city,address,phone_numberが存在すれば購入できる' do
-        expect(@order).to be_valid
-      end
-    end
-    context '商品購入がうまくいかないとき' do
-      it 'postal_codeが空だとうまくいかない' do
-        @order.postal_code = nil
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Postal code can't be blank")
-      end
-      it 'prefecture_idが1以外だとうまくいかない' do
-        @order.prefecture_id = 1
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Prefecture id Select")
-      end
-      it 'prefecture_idが空だとうまくいかない' do
-        @order.prefecture_id = nil
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Prefecture id can't be blank")
-      end
-      it 'cityが空だとうまくいかない' do
-        @order.city = nil
-        @order.valid?
-        expect(@order.errors.full_messages).to include("City can't be blank")
-      end
-      it 'addressが空だとうまくいかない' do
-        @order.address = nil
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Address can't be blank")
-      end
-      it 'phone_numberが空だとうまくいかない' do
-        @order.phone_number = nil
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Phone number can't be blank")
-      end
-      it 'postal_codeにはハイフンがないとうまくいかない' do
-          @order.prefecture_id = 1234567
-          @order.valid?
-          expect(@order.errors.full_messages).to include("Postal code into '-' ")
-        end
-      end
-      it 'phone_numberに数字以外があるとうまくいかない' do
-        
-      end
-    end
-  end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
