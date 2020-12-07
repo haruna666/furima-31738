dc7f29c0c5c6cd05a4d3ef318639cc2c399f4d28
haruna666 <sumiti6hal@gmail.com>
2020-12-07 20:38:49 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-	before_action :authenticate_user!, only: [:create]
+	before_action :authenticate_user!, only: [:create, :index]
 	before_action :set_order_item
 
 	def index
